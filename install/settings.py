94e4d3df668ddc86635098302767a15bd16d246c
Juan <coutinholima.j@gmail.com>
2024-05-28 11:21:48 -0300

    update login url

--- install/settings.py
+++ install/settings.py
@@ -134,3 +134,6 @@ MEDIA_ROOT = DATA_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
